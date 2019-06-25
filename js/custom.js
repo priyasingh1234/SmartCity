9a5c2bb27544fbe5b4e48802ded78727ed03e820
Akshat Anurag <akshatanurag1998@gmail.com>
2019-06-25 14:04:09 +0530

    fixed navbar in index_test

--- js/custom.js
+++ js/custom.js
@@ -21,18 +21,18 @@ $(window).on('load', function() {
 
 
 });
-$(document).ready(function(){
-$('.navbar-nav .nav-link').click(function(){
-	console.log($('.navbar-nav .nav-link'));
-    $('.navbar-nav .nav-link').removeClass('active');
-    $(this).addClass('active');
-});
+// $(document).ready(function(){
+// $('.navbar-nav .nav-link').click(function(){
+// 	console.log($('.navbar-nav .nav-link'));
+//     $('.navbar-nav .nav-link').removeClass('active');
+//     $(this).addClass('active');
+// });
 
-});
-document.querySelectorAll('.dropdown.nav-item').forEach(el => {
-	el.addEventListener('mouseleave', async() => {
-          await el.querySelector('.dropdown-menu.show')
-          await el.classList.remove('show')
+// });
+// document.querySelectorAll('.dropdown.nav-item').forEach(el => {
+// 	el.addEventListener('mouseleave', async() => {
+//           await el.querySelector('.dropdown-menu.show')
+//           await el.classList.remove('show')
 
-	});
-});
+// 	});
+// });
